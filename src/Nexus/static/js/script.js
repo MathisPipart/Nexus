2904840d6d2ce5134e28b1de291eeb6f619b0a3d
Pierro13 <pierre.alla13@gmail.com>
2023-10-14 11:42:46 +0200

    reorganisation + background + heure et date en cours

---
+++ src/Nexus/static/js/script.js
@@ -0,0 +1,25 @@
+alert("Hello, Welcome to Javatpoint");  
+
+function updateDateTime() {
+    const currentDateElement = document.getElementById('current-date');
+    const currentTimeElement = document.getElementById('current-time');
+
+    const currentDate = new Date().toLocaleDateString('fr-FR', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    });
+    const currentTime = new Date().toLocaleTimeString('fr-FR', {
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+
+    currentDateElement.innerText = currentDate;
+    currentTimeElement.innerText = currentTime;
+}
+
+// Mettre à jour l'heure et la date initialement
+updateDateTime();
+
+// Mettre à jour l'heure et la date toutes les secondes
+setInterval(updateDateTime, 1000);
