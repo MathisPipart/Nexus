01ac160aec08f9aec30feb85798520d190e5075f
Pierro13 <pierre.alla13@gmail.com>
2023-11-02 16:35:54 +0100

    update

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import Feed
 
 # Create your views here.
 def home(request):
     return render(request, "feed.html")
 
+def my_view(request):
+    feed = Feed(["Bonjour, il s'agit d'un modèle de post", "Pierre va payer sa tournée de cookies gourmands", "Il faudrait commencer à bosser pour les examens et avancer sur Schooding"])
+    return render(request, 'feed.html', {'feed': feed})
