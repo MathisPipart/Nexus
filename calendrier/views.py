21f704ff87734e7553472eacfc3da48c41aed408
lombarma <maxime.lombardo2@edu.esiee.fr>
2023-11-03 14:39:35 +0100

    first version of the calendar

--- calendrier/views.py
+++ calendrier/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from icalendar import Calendar
+import requests
+from .models import Event
+from django.utils import timezone
 
 # Create your views here.
 def vueCalendrier(request):
-    return render(request, "calendrier.html")
+    events = Event.objects.all()
+    return render(request, "calendrier.html", {"events": events})
+
+def importEvent(url):
+    req = requests.get(url)
+    cal = Calendar.from_ical(req.text)
+
+    for component in cal.walk():
+        if component.name == "VEVENT":
+            Event.objects.create(
+                title = component.get('summary'),
+                description = component.get('description', ''),
+                start_time = component.get('dtstart').dt,
+                end_time = component.get('dtend').dt,
+            )
+
+
+
+
+
