5300ba086bedc0d6bbd1405be993a783f5dfa738
lombarma <maxime.lombardo2@edu.esiee.fr>
2023-11-03 19:41:27 +0100

    Requirements.txt + ajout calendrier

--- calendrier/views.py
+++ calendrier/views.py
@@ -6,8 +6,18 @@ from django.utils import timezone
 
 # Create your views here.
 def vueCalendrier(request):
+    # URL du calendrier iCal fournie
+    ical_url = "https://planif.esiee.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=4195&projectId=11&calType=ical&nbWeeks=4"
+
+    # Importer les événements depuis l'URL iCal
+    importEvent(ical_url)
+
+    # Récupérer les événements depuis la base de données
     events = Event.objects.all()
+
+    # Envoyer les événements au template
     return render(request, "calendrier.html", {"events": events})
+
 
 def importEvent(url):
     req = requests.get(url)
@@ -15,12 +25,16 @@ def importEvent(url):
 
     for component in cal.walk():
         if component.name == "VEVENT":
-            Event.objects.create(
-                title = component.get('summary'),
-                description = component.get('description', ''),
-                start_time = component.get('dtstart').dt,
-                end_time = component.get('dtend').dt,
+            # Vérifiez si l'événement existe déjà pour éviter les doublons
+            _, created = Event.objects.get_or_create(
+                title=component.get('summary'),
+                defaults={
+                    'description': component.get('description', ''),
+                    'start_time': component.get('dtstart').dt,
+                    'end_time': component.get('dtend').dt,
+                }
             )
+
 
 
 
