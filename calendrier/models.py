38049f8f0548cc412e46960abfbaafaf47fa10b4
lombarma <maxime.lombardo2@edu.esiee.fr>
2023-11-25 19:35:46 +0100

    update of calendar

--- calendrier/models.py
+++ calendrier/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
+    title = models.TextField() # 4I-IN1:TD
+    description = models.TextField() # BUREAU D
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
