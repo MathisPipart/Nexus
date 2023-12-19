1e98dbe47a196e3d035a54191ba874766b6f2515
mathi <mathi@SPECTRE_MATHIS>
2023-12-19 10:20:24 +0100

    Classement en fonction de la BD

--- classement/admin.py
+++ classement/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Utilisateur
+from .models import Club
 
 # Register your models here.
+admin.site.register(Utilisateur)
+admin.site.register(Club)
--- classement/views.py
+++ classement/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Utilisateur
+from .models import Club
 
 # Create your views here.
 def vueClassement(request):
-    return render(request, "classement.html")
+    utilisateurs = Utilisateur.objects.all().order_by('-score')
+    clubs = Club.objects.all().order_by('-score')
+    context = {
+        'utilisateurs': utilisateurs,
+        'premier_utilisateur': utilisateurs[0] if len(utilisateurs) > 0 else None,
+        'deuxieme_utilisateur': utilisateurs[1] if len(utilisateurs) > 1 else None,
+        'troisieme_utilisateur': utilisateurs[2] if len(utilisateurs) > 2 else None,
+
+        'clubs': clubs,
+        'premier_club': clubs[0] if len(clubs) > 0 else None,
+        'deuxieme_club': clubs[1] if len(clubs) > 1 else None,
+        'troisieme_club': clubs[2] if len(clubs) > 2 else None,
+    }
+    return render(request, "classement.html", context)
