90b35067701b4ce0f9b63ff874ddccbe8293ed0d
Pierro13 <pierre.alla13@gmail.com>
2024-03-22 14:19:39 +0100

    Calendrier casi fini manque que les anciens cours mais osef blc

---
+++ calendrier/static/js/calendrier.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+        const calendarEl = document.getElementById('calendar');
+        let calendar = new FullCalendar.Calendar(calendarEl, {
+        timeZone: 'europe/Paris',
+        initialView: 'timeGridWeek',
+        height: 'auto',
+        locale: 'fr',
+        hiddenDays: [ 0 ], // Masquer les dimanches
+        nowIndicator: true, // Afficher la ligne actuelle
+        headerToolbar: {
+            left: 'prev,next,today',
+            center: 'title',
+            right: 'dayGridDay,timeGridWeek,dayGridMonth'
+        },
+        slotLabelFormat: {
+            hour: 'numeric',
+            minute: '2-digit',
+            omitZeroMinute: true,
+            meridiem: 'short'
+        },
+        slotMinTime: '07:00:00', // Heure de début de la grille de temps
+        slotMaxTime: '20:00:00', // Heure de fin de la grille de temps
+        scrollTime: '07:00:00', // Définir le défilement initial à 7h du matin
+        editable: false,
+        events: evenements
+    });
+calendar.render();
+});
