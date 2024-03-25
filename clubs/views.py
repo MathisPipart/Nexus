33e194539a5e2f46ddd39d5c834657ed4aac5196
Pierro13 <pierre.alla13@gmail.com>
2024-03-25 09:28:17 +0100

    push de base tmtc

--- clubs/views.py
+++ clubs/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def vueClubs(request):
-    return render(request, "clubs.html")
+    def vueClubs(request):
+        return render(request, "clubs.html")
+
+def club_musique(request):
+    return render(request, "club_musique.html")
