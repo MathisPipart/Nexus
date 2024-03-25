e6a824a3bb63739c2f4d8a202c995ea063c22773
maxime.lombardo <maxime.lombardo2@edu.esiee.fr>
2024-03-25 17:51:43 +0100

    Login page with google auth and stylized button

--- Nexus/settings.py
+++ Nexus/settings.py
@@ -179,6 +179,11 @@ CKEDITOR_CONFIGS = {
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
