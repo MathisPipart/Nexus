f08173186bb949ef261d68b3fd877e23b86f3c04
Pierro13 <pierre.alla13@gmail.com>
2024-03-25 09:54:09 +0100

    début

--- clubs/views.py
+++ clubs/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def vueClubs(request):
-    def vueClubs(request):
-        return render(request, "clubs.html")
+    return render(request, "clubs.html")
 
 def club_musique(request):
     return render(request, "club_musique.html")
