1cec98013228ca88ddf3b1101875cc86cab02e1b
maxime.lombardo <maxime.lombardo2@edu.esiee.fr>
2024-04-16 17:14:01 +0200

    Ajout des rôles dans la base. Début des confitions pour chaque utilisateur

--- login/models.py
+++ login/models.py
@@ -4,6 +4,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db import models
 
+
+class UserRole(models.TextChoices):
+    USER = 'user', 'User'
+    SUPERUSER = 'superuser', 'Superuser'
+    MODERATOR = 'moderator', 'Moderator'
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     url = models.URLField(blank=True, null=True)
+    role = models.CharField(
+        max_length=20,
+        choices=UserRole.choices,
+        default=UserRole.USER
+    )
+
