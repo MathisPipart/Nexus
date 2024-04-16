ee8830eee990a0421c6903df5482ca4bb2d63fa5
Pierro13 <pierre.alla13@gmail.com>
2024-04-16 11:07:41 +0200

    petite avancée avec le club muqieu en bdd

--- clubs/models.py
+++ clubs/models.py
@@ -10,12 +10,14 @@ class Info_Clubs(models.Model):
     president = models.CharField(max_length=50)
     vp1 = models.CharField(max_length=50)
     vp2 = models.CharField(max_length=50)
-    vp3 = models.CharField(max_length=50)
+    vp3 = models.CharField(max_length=50, blank=True)
     secretaire = models.CharField(max_length=50)
-    secretaire2 = models.CharField(max_length=50)
+    secretaire2 = models.CharField(max_length=50, blank=True)
     tresorier = models.CharField(max_length=50)
-    tresorier2 = models.CharField(max_length=50)
+    tresorier2 = models.CharField(max_length=50, blank=True)
 
     
     def __str__(self):
+        return f"{self.nom} - {self.description}"
+
         return self.nom + " " + self.description + " " + self.banniere + " " + self.photo + " " + self.president + " " + self.vp1 + " " + self.vp2 + " " + self.secretaire + " " + self.tresorier
