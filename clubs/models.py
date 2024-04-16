7f8d60562f5b140b3be8ed6ba1b15f5a441d51ca
Pierro13 <pierre.alla13@gmail.com>
2024-04-16 10:55:05 +0200

    début de model d'info de sclubs

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Info_Clubs(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=50)
+    description = models.TextField()
+    banniere = models.ImageField(upload_to='clubs/')
+    photo = models.ImageField(upload_to='clubs/')
+    president = models.CharField(max_length=50)
+    vp1 = models.CharField(max_length=50)
+    vp2 = models.CharField(max_length=50)
+    vp3 = models.CharField(max_length=50)
+    secretaire = models.CharField(max_length=50)
+    secretaire2 = models.CharField(max_length=50)
+    tresorier = models.CharField(max_length=50)
+    tresorier2 = models.CharField(max_length=50)
+
+    
+    def __str__(self):
+        return self.nom + " " + self.description + " " + self.banniere + " " + self.photo + " " + self.president + " " + self.vp1 + " " + self.vp2 + " " + self.secretaire + " " + self.tresorier
--- clubs/views.py
+++ clubs/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from feed.models import Post 
 from django.shortcuts import render
+from .models import Info_Clubs
 
 
 def vueClubs(request):
-    return render(request, "clubs.html")
+    clubs = Info_Clubs.objects.all()
+    return render(request, "clubs.html", {'clubs': clubs})
 
 
 def club_details(request, club_type):
