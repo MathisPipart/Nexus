0aa7aa90f9f4923e50fd1d8fd0d14cd835486013
Pierro13 <pierre.alla13@gmail.com>
2024-04-16 15:34:28 +0200

    bete d'avencement

--- clubs/models.py
+++ clubs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms.models import model_to_dict
 
 # Create your models here.
 class Info_Clubs(models.Model):
@@ -16,8 +17,9 @@ class Info_Clubs(models.Model):
     tresorier = models.CharField(max_length=50)
     tresorier2 = models.CharField(max_length=50, blank=True)
 
-    
     def __str__(self):
-        return f"{self.nom} - {self.description}"
+        return self.nom + " " + self.description + " " + self.president + " " + self.vp1 + " " + self.vp2 + " " + self.secretaire + " " + self.tresorier
 
-        return self.nom + " " + self.description + " " + self.banniere + " " + self.photo + " " + self.president + " " + self.vp1 + " " + self.vp2 + " " + self.secretaire + " " + self.tresorier
+    def get_all_fields(self):
+        return model_to_dict(self)
+
--- clubs/views.py
+++ clubs/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from feed.models import Post 
-from django.shortcuts import render
 from .models import Info_Clubs
 
 
 def vueClubs(request):
     clubs = Info_Clubs.objects.all()
-    return render(request, "clubs.html", {'clubs': clubs})
+
+    context = {
+        'clubs': clubs,
+    }
+
+    return render(request, "clubs.html", context)
 
 
 def club_details(request, club_type):
+    clubs = Info_Clubs.objects.all().filter(nom=club_type).first()
+
     posts = Post.objects.all().order_by('-date_de_creation')
     posts_size = len(posts)
     posts = reversed(posts)
-    return render(request, "club_details.html", {'posts': posts, 'posts_size': posts_size, 'club_type': club_type})
+
+    context = {
+        'clubs': clubs,
+        'posts': posts, 
+        'posts_size': posts_size, 
+    }
+
+    return render(request, "club_details.html", context)
