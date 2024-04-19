0ba40a7f446dc7cbcc49a53baa986decac6bdc5a
maxime.lombardo <maxime.lombardo2@edu.esiee.fr>
2024-04-19 17:11:59 +0200

    updated userprofile

--- login/models.py
+++ login/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserRole(models.TextChoices):
@@ -23,3 +25,11 @@ class UserProfile(models.Model):
     def get_subscribed_clubs(self):
         return self.user.clubs_subscribed.all()
 
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+post_save.connect(create_user_profile, sender=User)
